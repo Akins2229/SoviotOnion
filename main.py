4e3a6e5c36831dcceb5f54f268e975a850650c84
Akins <82357502+Akins2229@users.noreply.github.com>
2021-05-17 16:47:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -127,6 +127,84 @@ async def levels(ctx):
 			break 
 	await ctx.send(embed=embed)	
 
+@bot.group(invoke_without_command=True)
+async def help(ctx):
+  embed=discord.Embed(title="Help Command")
+  embed.add_field(name="Music", value="Category for Music Commands", inline=False)
+  embed.add_field(name="Moderation", value="Category for Moderation Commands", inline=False)
+  embed.add_field(name="Fun", value="Category for Fun Commands", inline=False)
+  embed.add_field(name="Economy", value="Category for Economy Commands", inline=False)
+  embed.add_field(name="Gambling", value="Category for Gambling Commands", inline=False)
+  await ctx.send(embed=embed)
+
+@help.command()
+async def economy(ctx):
+  embed=discord.Embed(title="Economy Help Command", color=0xfeadad)
+  embed.add_field(name="Work", value="You will work for money; Usage: {prefix}work", inline=False)
+  embed.add_field(name="Slut", value="You will do something slutty for money; Usage:{prefix}slut", inline=False)
+  embed.add_field(name="Crime", value="You will commit a crime for money; Usage:{prefix}crime", inline=False)
+  embed.add_field(name="Balance", value="Ascertains a members balance; Usage:{prefix}balance {member: optional}", inline=False)
+  embed.add_field(name="Leaderboard", value="Displays the top 5 richest members; Usage:{prefix}leaderboard", inline=False)
+  await ctx.send(embed=embed)
+
+@help.command()
+async def music(ctx):
+  embed=discord.Embed(title="Music Help Command")
+  embed.add_field(name="Play", value="Will play a given video; Usage:{prefix}play {song}", inline=False)
+  embed.add_field(name="Now", value="Will display the currently playing video; Usage:{prefix}now", inline=False)
+  embed.add_field(name="Queue", value="Will display the next 10 videos; Usage:{prefix}queue {page: optional}", inline=False)
+  embed.add_field(name="Skip", value="Will skip the currently playing song; Usage:{prefix}skip", inline=False)
+  embed.add_field(name="Remove", value="Will remove a song from the queue; Usage:{prefix}remove {song number}", inline=False)
+  embed.add_field(name="Stop", value="Will stop the current song, clear the queue, and leave the channel; Usage:{prefix}stop", inline=False)
+  embed.add_field(name="Pause", value="Will pause the current song; Usage:{prefix}pause", inline=False)
+  embed.add_field(name="Resume", value="Will resume the current song (if paused); Usage:{prefix}resume", inline=False)
+  embed.add_field(name="Volume", value="Will change the current volume of the bot; Usage:{prefix}volume {int: 1-100}", inline=False)
+  embed.add_field(name="Join", value="Will have the bot join your current voice channel; Usage:{prefix}join", inline=False)
+  await ctx.send(embed=embed)
+
+@help.command()
+async def gambling(ctx):
+  embed=discord.Embed(title="Gambling Help Command")
+  embed.add_field(name="Slots", value="Will have you bet on the slot machine; Usage:{prefix}slots {bet: int>100}", inline=False)
+  embed.add_field(name="Cockfight", value="Will have you bet on a cockfight; Usage:{prefix}cockfight {bet: int>100}", inline=False)
+  await ctx.send(embed=embed)
+
+@help.command()
+async def fun(ctx):
+  embed=discord.Embed(title="Fun Help Command")
+  embed.add_field(name="badtake", value="Displays one of many badtakes that I have; Usage:{prefix}badtake", inline=False)
+  embed.add_field(name="acab", value="Displays my opinion on ACAB; Usage:{prefix}acab", inline=True)
+  embed.add_field(name="blm", value="Displays my opinion of BLM; Usage:{prefix}blm", inline=True)
+  embed.add_field(name="Respect", value="Will ascertain whether or not I respect something; Usage:{prefix}respect {argument}", inline=True)
+  embed.add_field(name="Say", value="Will have the bot say something; Usage:{prefix}say {message}", inline=True)
+  embed.add_field(name="Anarkiddy", value="Anarchists are cringe, the voices of the heavens above (myself) will tell you such; Usage:{prefix}anarkiddy", inline=True)
+  embed.add_field(name="Google", value="Will google something; Usage:{prefix}google \"{query}\" {limit: int (Optional)}", inline=True)
+  embed.add_field(name="Define", value="Will define a given word; Usage:{prefix}define {word}", inline=True)
+  embed.add_field(name="Translate", value="Will translate a given word to English; Usage:{prefix}translate {word}", inline=True)
+  embed.add_field(name="Translatedefine", value="Will define a word from another language; Usage:{prefix}translatedefine {word}", inline=True)
+  embed.add_field(name="Level", value="Will give you the member of a member; Usage:{prefix}level {member: optional}", inline=True)
+  embed.add_field(name="Levels", value="Will display the 5 highest levels; Usage:{prefix}levels", inline=True)
+  embed.add_field(name="Transgender", value="Voices my opinion on Trans Rights; Usage:{prefix}transgender", inline=True)
+  embed.add_field(name="Thesaurus", value="Will display synonyms and antonyms for a given word; Usage:{prefix}thesaurus {word{", inline=True)
+  embed.set_footer(text="Note: Any and all words spoken by this bot, from any commands in this cog are not liable to Akins")
+  await ctx.send(embed=embed)
+
+@help.command()
+async def moderation(ctx):
+  embed=discord.Embed()
+  embed.add_field(name="Ban", value="Will ban a user; Usage:{prefix}ban {member} {reason: optional}}", inline=False)
+  embed.add_field(name="Kick", value="Will kick a user; Usage:{prefix}kick {member} {reason: optional}", inline=True)
+  embed.add_field(name="Mute", value="Will mute a user; Usage:{prefix}mute {member} {duration: minutes} {reason: optional}", inline=True)
+  embed.add_field(name="Unmute", value="Will unmute a user; Usage:{prefix}unmute {member}", inline=True)
+  embed.add_field(name="Hackban", value="Will ban, then unban a user; Usage:{prefix}hackban {member} {reason: optional}", inline=True)
+  embed.add_field(name="Softban", value="Will ban a user for a given time; Usage:{prefix}softban {member} {duration: minutes} {reason: optional}", inline=True)
+  embed.add_field(name="Unban", value="Will unban a user; Usage:{prefix}unban {member}", inline=True)
+  embed.add_field(name="Userinfo", value="Will give information on a user; Usage:{prefix}userinfo {member: optional}", inline=True)
+  embed.add_field(name="Avatar", value="Will display a users avatar; Usage:{prefix}avatar {member: optional}", inline=True)
+  embed.add_field(name="Purge", value="Will purge a given number of messages; Usage:{prefix}purge {limit: int}", inline=True)
+  embed.set_footer(text="All of these commands require their respective permissions")
+  await ctx.send(embed=embed)
+
 # load cogs & run bot
 if __name__ == '__main__':
   for cog in cogs:
